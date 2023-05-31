ff79e606195d59fc8a37b32eae9fb8fb0e282510
Tani Gross <jgross5@mail.yu.edu>
2023-05-31 12:39:59 -0400

    fixed API

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-name = input("What's your name? ")
 
 app = FastAPI()
 
-
-
 @app.get("/")
 async def root():
+    return {"message": "Default (No Name)" }
+
+@app.get("/{name}")
+async def return_name(name):
     return {"message": "Hello, " + name}
